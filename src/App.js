e393e8ab1055cedcf808248605ea3f79db5abc59
Matt <moggiex@gmail.com>
2022-02-26 08:56:45 +0000

    22:00

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 // import logo from './logo.svg';
 import './App.css';
 
+// Components
+import TourCard from './components/TourCard/TourCard'
+
+// Material Ui
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+
+
 function App() {
   return (
     <div className="App">
+      <Container>
 
+        <Grid container spacing={2}>
+          <TourCard />
+          <TourCard />
+          <TourCard />
+        </Grid>
+
+      </Container>
     </div>
   );
 }
---
+++ src/components/TourCard/TourCard.jsx
@@ -0,0 +1,12 @@
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+
+const TourCard = () => {
+	return (
+		<Grid item xs={4}>
+			<Paper elevation={3}>THello</Paper>
+		</Grid>
+	);
+};
+
+export default TourCard;
